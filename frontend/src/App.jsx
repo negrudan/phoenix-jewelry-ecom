440338c43fb097d89db1fa48a0ce2616a63bf74f
unknown <dan.negru2002@gmail.com>
2023-04-29 02:55:08 +0300

    Initial Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+import MainPage from "./components/MainPage";
+
+export default function App() {
+  return (
+    <>
+      <React.Fragment>
+        <NavBar />
+        <MainPage />
+      </React.Fragment>
+    </>
+  );
+}
