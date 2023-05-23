8e8f71a98d031b4d6c2fc6fea14969a04430ee0f
unknown <dan.negru2002@gmail.com>
2023-05-23 19:35:05 +0300

    Implement footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,8 +2,66 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="footer">
-      <div className="text-center">All rights reserved</div>
+    // <!-- Footer -->
+    <footer class="text-center text-lg-start bg-dark text-white mt-5">
+      {/* <!-- Section: Links  --> */}
+      <section class="">
+        <div class="container text-center text-md-start mt-5">
+          {/* <!-- Grid row --> */}
+          <div class="row mt-3">
+            {/* <!-- Grid column --> */}
+            <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4 text-center">
+              {/* <!-- Content --> */}
+              <h6 class="text-uppercase fw-bold mb-4">Social</h6>
+              <p>Check my work</p>
+              <div>
+                <a href="#!" class="btn btn-dark btn-lg">
+                  <i class="fab fa-linkedin"></i>
+                </a>
+                <a href="#!" class="btn btn-dark btn-lg">
+                  <i class="fab fa-github"></i>
+                </a>
+              </div>
+            </div>
+            {/* <!-- Grid column --> */}
+
+            {/* <!-- Grid column --> */}
+
+            {/* <!-- Grid column --> */}
+
+            {/* <!-- Grid column --> */}
+
+            {/* <!-- Grid column --> */}
+
+            {/* <!-- Grid column --> */}
+            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+              {/* <!-- Links --> */}
+              <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+              <p>
+                <i class="fas fa-envelope me-3 text-secondary mb-2"></i>
+                dan.negru2002@gmail.com
+              </p>
+              <p>
+                <i class="fas fa-phone me-3 text-secondary"></i> (+40) 0773 307
+                342
+              </p>
+            </div>
+            {/* <!-- Grid column --> */}
+          </div>
+          {/* <!-- Grid row --> */}
+        </div>
+      </section>
+      {/* <!-- Section: Links  --> */}
+
+      {/* <!-- Copyright --> */}
+      <div class="text-center p-4 footer">
+        ©2023 Copyright:&nbsp;
+        <a class="text-reset fw-bold" href="#!">
+          MY PORTFOLIO
+        </a>
+      </div>
+      {/* <!-- Copyright --> */}
     </footer>
+    // <!-- Footer -->
   );
 }
